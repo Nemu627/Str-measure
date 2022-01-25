413288a87cfbaa12861c94cfa4c872f77f176072
音夢?! <82650472+Nemu627@users.noreply.github.com>
2022-01-25 22:17:00 +0900

    fix : Bug fixes

--- measure/main.py
+++ measure/main.py
@@ -5,15 +5,15 @@ def get_length(message):
     for i in message:
         letter = unicodedata.east_asian_width(i)
         if letter == 'H':
-            string_length =+ 1
+            string_length += 1
         elif letter == 'Na':
-            string_length =+ 1
+            string_length += 1
         elif letter == 'F':
-            string_length =+ 2
+            string_length += 2
         elif letter == 'A':
-            string_length =+ 2
+            string_length += 2
         elif letter == 'W':
-            string_length =+ 2
+            string_length += 2
         else:
-            string_length =+ 1
+            string_length += 1
     return string_length
